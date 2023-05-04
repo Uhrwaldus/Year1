df40f3be18f7d198a95fa0f85a605c2c9ed526f4
Uhrwaldus <CasperUKristensen@gmail.com>
2023-05-04 11:20:56 +0200

    MavenBuild

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        //connect til database
+        DataAccess db = new DataAccess();
+        db.Connect();
+    }
+}
