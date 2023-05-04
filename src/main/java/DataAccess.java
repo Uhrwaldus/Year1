45c14e05ff4934669163cf2ab1e0977b6548582e
Uhrwaldus <CasperUKristensen@gmail.com>
2023-05-04 10:58:56 +0200

    Update
    Updatede til maven build path for at fixe connection string.
    
    Co-Authored-By: Ulrikke <115539901+Zaphina@users.noreply.github.com>
    Co-Authored-By: jonpe01 <131650096+jonpe01@users.noreply.github.com>

---
+++ src/main/java/DataAccess.java
@@ -0,0 +1,35 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataAccess {
+
+    public boolean Connect() throws SQLException {
+
+        String connectionString =
+                "jdbc:sqlserver://localhost:1433;" +
+                        "databaseName=JonasLugter;" +
+                        "user=SA;" +
+                        "password=Fisk1234;" +
+                        "encrypt=true;" +
+                        "trustServerCertificate=true;";
+
+        Connection connection = null;
+
+        try {
+            System.out.println("Connecting to database...");
+
+            connection = DriverManager.getConnection(connectionString);
+
+            System.out.println("Connected to database");
+
+            return true;
+        }
+        catch (SQLException e) {
+            System.out.println("Could not connect to database!");
+            System.out.println(e.getMessage());
+
+            return false;
+        }
+    }
+}
