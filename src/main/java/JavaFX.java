f1bde05e0cf6d1317d32a13a87902878a043100a
Ulrikke <ulrikkesten@hotmail.com>
2023-05-08 14:52:33 +0200

    Broken JavaFX

---
+++ src/main/java/JavaFX.java
@@ -0,0 +1,28 @@
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class JavaFX extends Application {
+    /*
+    public static void main(String[] args) throws SQLException {
+        //connect til database
+        DataAccess db = new DataAccess();
+        db.Connect();
+
+    }
+    */
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(JavaFX.class.getResource("Home.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+
+        stage.setTitle("Home");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
