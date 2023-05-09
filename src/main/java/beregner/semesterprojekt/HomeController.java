863f3d55c48fc1a70e23a043b7f7548b6e16752a
Uhrwaldus <CasperUKristensen@gmail.com>
2023-05-09 11:51:24 +0200

    Full rebuild

---
+++ src/main/java/beregner/semesterprojekt/HomeController.java
@@ -0,0 +1,19 @@
+package beregner.semesterprojekt;
+
+import javafx.fxml.FXML;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class HomeController {
+    int udbetaling, indkomst, bil, pris, periode;
+
+    @FXML
+    private TextField udbetalinginput;
+
+    public void check(ActionEvent event ){
+        udbetaling = Integer.parseInt(udbetalinginput.getText());
+        System.out.println(udbetaling);
+    }
+
+}
