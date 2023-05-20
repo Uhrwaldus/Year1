302193e72516236730eb022693c5c6403e38b33a
Uhrwald <Casper007113@gmail.com>
2023-05-20 11:12:27 +0200

    small fix, runnable

--- src/main/java/beregner/semesterprojekt/StatController.java
+++ src/main/java/beregner/semesterprojekt/StatController.java
@@ -1,4 +1,15 @@
 package beregner.semesterprojekt;
 
-public class StatController {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class StatController implements Initializable {
+    private StatModel database;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
 }
--- src/main/java/beregner/semesterprojekt/StatModel.java
+++ src/main/java/beregner/semesterprojekt/StatModel.java
@@ -1,4 +1,38 @@
 package beregner.semesterprojekt;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class StatModel {
+    private Connection connection;
+    public boolean Connect() throws SQLException {
+
+        String connectionString =
+                "jdbc:sqlserver://localhost:1433;" +
+                        "databaseName=FerrariDB;" +
+                        "user=SA;" +
+                        "password=Fisk1234;" +
+                        "encrypt=true;" +
+                        "trustServerCertificate=true;";
+
+        connection = null;
+
+        try {
+            System.out.println("Connecting to database...");
+
+            connection = DriverManager.getConnection(connectionString);
+
+            System.out.println("Connected to database");
+
+            return true;
+        }
+        catch (SQLException e) {
+            System.out.println("Could not connect to database!");
+            System.out.println(e.getMessage());
+
+            return false;
+        }
+    }
+
 }
